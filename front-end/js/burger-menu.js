baeb2325b4f6eb82131711d0e88ae76cea0182c1
YungDavo <davidcbne@gmail.com>
2021-07-24 17:37:06 +0200

    Product page improved: loading animations added, preview product animation added

--- front-end/js/burger-menu.js
+++ front-end/js/burger-menu.js
@@ -31,4 +31,24 @@ links.forEach((link) => {
   });
 });
 
-console.log(animLink[0]);
+const previewLink = document.querySelector(".article__preview--button");
+const previewLinkContainer = document.querySelector(
+  ".article__preview--button--container__anim"
+);
+const previewLinkCta = document.querySelector(
+  ".article__preview--button--container"
+);
+const previewIcon = document.querySelector(".article__preview--icon");
+const previewProduct = document.querySelector(".section__dyn");
+
+previewLink.addEventListener("click", function () {
+  previewLinkContainer.classList.toggle(
+    "article__preview--button--container__toggle"
+  );
+  previewLinkCta.classList.toggle(
+    "article__preview--button--container__toggle--cta"
+  );
+  previewIcon.classList.toggle("article__preview--icon__toggle");
+  previewProduct.classList.toggle("section__dyn__toggle");
+});
+previewLink();
